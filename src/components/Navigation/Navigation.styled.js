5dbb17dff53a8d488dba5a9cbec0312fd2a69bd4
anglsom <anastasiyaladkovskaya@gmail.com>
2022-11-12 13:18:27 +0100

    update

--- src/components/Navigation/Navigation.styled.js
+++ src/components/Navigation/Navigation.styled.js
@@ -5,7 +5,7 @@ export const NavigationLink = styled(NavLink)`
   display: inline-block;
   text-decoration: none;
   font-weight: 500;
-  color: black;
+  color: pink;
   font-size: 18px;
   &:hover {
     color: pink;
